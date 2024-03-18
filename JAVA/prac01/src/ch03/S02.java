0a498e80b1d90721cc8a2fc3197c0c8e030b3f5d
jmh <whaksgus123@gmail.com>
2024-03-18 16:40:48 +0900

    : add S02.java
    java 수업 내용 추가.

---
+++ JAVA/prac01/src/ch03/S02.java
@@ -0,0 +1,15 @@
+package ch03;
+/*
+class : S02
+    - Method : main
+        . 선생님 블로그 ( blog.naver.com/drv98 - 자바시작하기 - 연습문제 2번 )
+ */
+public class S02 {
+	public static void main(String[] args) {
+		int math = 96;
+		int science = 88;
+		int eng = 76;
+
+		System.out.printf("수학 : %d \n과학 : %d \n영어 : %d", math, science, eng);
+	}
+}
