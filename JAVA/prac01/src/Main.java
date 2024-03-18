97a38863d08647c67769a2b6540f067dbe335c81
jmh <whaksgus123@gmail.com>
2024-03-18 11:43:20 +0900

    프로젝트 생성 및 수업내용

---
+++ JAVA/prac01/src/Main.java
@@ -0,0 +1,20 @@
+/*
+    - Class Main
+        = Method : Main
+            . 프로그램이 시작되는 함수.
+            . 기본적 선언 및 초기화를 한다.
+ */
+public class Main {
+    public static void main(String[] args) {
+        //자바는 메인 메소드에서 시작된다.
+        System.out.println("hello world");
+
+        // 변수 선언 및 초기화
+        int a = 1;
+        char b = 'c';
+        String c = "abc";
+
+        // 변수 확인용 출력
+        System.out.println("int a : " + a + " / char b : " + b + " / String c : " + c );
+    }
+}
