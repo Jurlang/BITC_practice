be2f1c39284110b10831222c01adee262d4e5506
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2024-03-19 14:05:00 +0900

    : modify java S_Generics
    제네릭 수업내용 추가

--- JAVA/prac01/src/ch03_dataType/S_Generics.java
+++ JAVA/prac01/src/ch03_dataType/S_Generics.java
@@ -18,10 +18,22 @@ public class S_Generics {
 		System.out.println("reverseOrder sort 후 : " + as);
 		System.out.println();
 
+		System.out.println("String배열을 ArrayList<String>에 Arrays.asList(StringArray) 로 넣기.");
+		String[] sa = {"a", "b", "c", "def", "efg"};
+		ArrayList<String> as1 = new ArrayList<>(Arrays.asList(sa));
+		System.out.println("StringArray sa : [a, b, c, def, efg]");
+		System.out.println("as1 = new ArrayList<>(Arrays.asList(sa))");
+		System.out.println("as1 : " + as1);
+		System.out.println();
+
 		ArrayList<String> as2 = new ArrayList<>(Arrays.asList("123", "234", "345"));
-		System.out.println("Arrays.asList 이용하여 as2 만들기 : " + as2);
+		System.out.println("Arrays.asList(여러개 값) 이용하여 as2 만들기.");
+		System.out.println("as2 = new ArrayList<>(Arrays.asList(\"123\", \"234\", \"345\")");
+		System.out.println("as2 : " + as2);
+		System.out.println();
+
 		String result = String.join(" : ", as2);
-		System.out.println("String.join(\" : \", as2) - result = " + result);
+		System.out.println("String.join(\" : \", as2) - String result = " + result);
 		System.out.println();
 
 		ArrayList<Integer> ai = new ArrayList<>();
