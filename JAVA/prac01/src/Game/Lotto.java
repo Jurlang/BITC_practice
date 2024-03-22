ae25fee0999e52e1cc78e48607b762f5f9aa1b03
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2024-03-22 16:28:10 +0900

    : add JAVA lotto
    로또 추첨 번호

---
+++ JAVA/prac01/src/Game/Lotto.java
@@ -0,0 +1,32 @@
+package Game;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class Lotto {
+	public static void main(String[] args) {
+		ArrayList<Integer>[] lotto = new ArrayList[5];
+		for(int i = 0 ; i < 5 ; i++){
+			lotto[i] = new ArrayList<>();
+		}
+		for(int i = 0; i< 5; i++){
+			for(int j = 0 ; j < 6 ; j++) {
+				int randomNum = (int) (Math.random() * 45 + 1);
+				if(lotto[i].contains(randomNum)){
+					j -= 1;
+				} else {
+					lotto[i].add(randomNum);
+				}
+			}
+			lotto[i].sort(Comparator.naturalOrder());
+		}
+		System.out.println("-------------------------------------");
+		System.out.println("T O D A Y   L O T T O   N U M B E R !");
+		System.out.println("-------------------------------------");
+		for(int i = 0; i < 5 ; i++){
+			System.out.println((i+1) + " : " +lotto[i]);
+		}
+		System.out.println("-------------------------------------");
+
+	}
+}
