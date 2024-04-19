25d7cee6ab4c58e6b8e4c4ab1c183a62b0e219a5
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2024-04-19 10:00:17 +0900

    세션 / 쿠키 수업을 위한 Login 페이지와 Controller 연결.

--- JavaWeb/w3/src/main/java/org/zerock/w3/w3/todo_1/contoller/LoginController.java
+++ JavaWeb/w3/src/main/java/org/zerock/w3/w3/todo_1/contoller/LoginController.java
@@ -7,6 +7,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @Log4j2
@@ -15,5 +16,18 @@ public class LoginController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		req.getRequestDispatcher("/login.jsp").forward(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String mid = req.getParameter("mid");
+		String mpw = req.getParameter("mpw");
+
+		String mSession = mid+mpw;
+
+		HttpSession s = req.getSession();
+		s.setAttribute("loginInfo", mSession);
+
+		resp.sendRedirect("todo/list");
 	}
 }
--- JavaWeb/w3/src/main/webapp/login.jsp
+++ JavaWeb/w3/src/main/webapp/login.jsp
@@ -5,5 +5,10 @@
 </head>
 <body>
 	<h1>Login Page</h1>
+	<form action="./login" method="post">
+		<input type="text" name="mid" id="mid" placeholder="아이디 입력란">
+		<input type="password" name="mpw" id="mpw" placeholder="비밀번호 입력란">
+		<button type="submit">로그인</button>
+	</form>
 </body>
 </html>
