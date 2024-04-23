4c6c4a6518e095712d4407703a990f0e26430480
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2024-04-23 17:18:46 +0900

    키보드 입력까지

--- JS/DrumKit/app.js
+++ JS/DrumKit/app.js
@@ -1,15 +1,37 @@
 const button = document.querySelector("button");
 const buttons = document.querySelectorAll(".drum");
-const arrName = { w: "crash", a: "kick-bass", s: "snare", d: "tom1", j: "tom2", k: "tom3", l: "tom4" };
+const arrName = { w: "tom1", a: "tom2", s: "tom3", d: "tom4", j: "snare", k: "crash", l: "kick-bass" };
 
 for (var i = 0; i < buttons.length; i++) {
-  buttons[i].style.background = `url(images/${arrName[buttons[i].className.split(" ")[0]]}.png)`;
+  buttons[i].style.background = `url(images/${arrName[buttons[i].className.split(" ")[0]]}.png) center no-repeat`;
+  buttons[i].style.backgroundSize = `cover`;
   buttons[i].addEventListener("click", function () {
-    handleClick(this);
+    mouseClick(this);
   });
 }
 
-function handleClick(idx) {
+function mouseClick(idx) {
   var audio = new Audio("sounds/" + arrName[idx.className.split(" ")[0]] + ".mp3");
+  console.log(arrName[idx.className.split(" ")[0]] + ".mp3");
   audio.play();
 }
+
+function keyboardClick(idx) {
+  switch (idx) {
+    case "w":
+    case "a":
+    case "s":
+    case "d":
+    case "j":
+    case "k":
+    case "l":
+      var audio = new Audio("sounds/" + arrName[idx] + ".mp3");
+      console.log(arrName[idx] + ".mp3");
+      audio.play();
+      break;
+    default:
+      break;
+  }
+}
+
+window.onkeydown = (e) => keyboardClick(e.key);
