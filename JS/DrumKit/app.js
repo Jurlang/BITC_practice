9cf68fc989464ea956c73efbacd58df1db3e832c
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2024-04-24 11:07:31 +0900

    드럼킷 최종

--- JS/DrumKit/app.js
+++ JS/DrumKit/app.js
@@ -5,17 +5,21 @@ const objName = { w: "tom1", a: "tom2", s: "tom3", d: "tom4", j: "snare", k: "cr
 for (var i = 0; i < buttons.length; i++) {
   buttons[i].style.background = `url(images/${objName[buttons[i].textContent]}.png) center no-repeat`;
   buttons[i].style.backgroundSize = `cover`;
-  buttons[i].addEventListener("click", function () {
-    mouseClick(this);
-  });
+  // buttons[i].addEventListener("click", function () {
+  //   mouseClick(this);
+  // });
+  buttons[i].onclick = (e) => mouseClick(e.srcElement);
 }
-
 function mouseClick(obj) {
   var audio = new Audio("sounds/" + objName[obj.textContent] + ".mp3");
-  console.log(objName[obj.textContent] + ".mp3");
+  obj.style.color = "blue";
+  obj.classList.add("pressed");
+  setTimeout(function () {
+    obj.classList.remove("pressed");
+    obj.style.color = "#da0463";
+  }, 200);
   audio.play();
 }
-
 function keyboardClick(idx) {
   switch (idx) {
     case "w":
@@ -25,13 +29,18 @@ function keyboardClick(idx) {
     case "j":
     case "k":
     case "l":
+      var btn = document.querySelector("." + idx);
+      btn.style.color = "blue";
+      btn.classList.add("pressed");
+      setTimeout(function () {
+        btn.classList.remove("pressed");
+        btn.style.color = "#da0463";
+      }, 200);
       var audio = new Audio("sounds/" + objName[idx] + ".mp3");
-      console.log(objName[idx] + ".mp3");
       audio.play();
       break;
     default:
       break;
   }
 }
-
 document.onkeydown = (e) => keyboardClick(e.key);
