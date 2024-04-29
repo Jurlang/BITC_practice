a1364649b642cff2a05b820f37b205c15c82fb45
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2024-04-29 16:45:05 +0900

    ?????

--- JavaWeb/springex/src/main/java/org/zerock/springex/sample/SampleService.java
+++ JavaWeb/springex/src/main/java/org/zerock/springex/sample/SampleService.java
@@ -1,4 +1,16 @@
 package org.zerock.springex.sample;
 
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service
+@ToString
+@RequiredArgsConstructor
 public class SampleService {
+
+	@Qualifier("event")
+	final private SampleDAO sampleDAO;
+
 }
