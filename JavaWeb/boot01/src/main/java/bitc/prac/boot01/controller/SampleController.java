2d6275838d737a341a24a2a47c876d05d30349d1
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2024-05-07 10:33:59 +0900

    Spring Boot Controller & html Setting - java - Controller 생성 - resources - templates/hello.html 생성   = 화면은 templates 폴더에

---
+++ JavaWeb/boot01/src/main/java/bitc/prac/boot01/controller/SampleController.java
@@ -0,0 +1,17 @@
+package bitc.prac.boot01.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Log4j2
+public class SampleController {
+
+	@GetMapping("/hello")
+	public String hello(Model model){
+		model.addAttribute("msg", "Spring Boot - Hello");
+		return "hello";
+	}
+}
