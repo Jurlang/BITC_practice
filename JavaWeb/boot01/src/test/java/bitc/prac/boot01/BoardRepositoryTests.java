5dc99920b7131f6935636f529a159cf800524e90
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2024-05-07 17:18:29 +0900

    JPA 사용 - Paging

--- JavaWeb/boot01/src/test/java/bitc/prac/boot01/BoardRepositoryTests.java
+++ JavaWeb/boot01/src/test/java/bitc/prac/boot01/BoardRepositoryTests.java
@@ -2,12 +2,16 @@ package bitc.prac.boot01;
 
 import bitc.prac.boot01.domain.Board;
 import bitc.prac.boot01.repository.BoardRepository;
-import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
+import java.util.List;
 import java.util.Optional;
 
 @SpringBootTest
@@ -57,5 +61,20 @@ public class BoardRepositoryTests {
 		Long bno = 1L;
 
 		boardRepository.deleteById(bno);
+	}
+
+	@Test
+	public void testPaging(){
+		Pageable pageable = PageRequest.of(1,10, Sort.by("bno").descending());
+		Page<Board> result = boardRepository.findAll(pageable);
+
+		log.info("total count : {}", result.getTotalElements());
+		log.info("total pages : {}", result.getTotalPages());
+		log.info("page number : {}", result.getNumber());
+		log.info("page size : {}", result.getSize());
+
+		List<Board> boards = result.getContent();
+
+		boards.forEach(log::info);
 	}
 }
