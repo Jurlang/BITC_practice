f7688dce0b7798265906e023362e032b8968e8d2
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2024-05-09 15:59:31 +0900

    boot3 SSB Example  - Update Test

--- JavaWeb/boot03/src/test/java/org/bitcprac/boot03/Boot03ApplicationTests.java
+++ JavaWeb/boot03/src/test/java/org/bitcprac/boot03/Boot03ApplicationTests.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class Boot03ApplicationTests {
@@ -55,5 +56,12 @@ class Boot03ApplicationTests {
 //		List<Question> qList = qRepo.findBySubjectLike("SBB%");
 //		Question q = qList.get(0);
 //		assertEquals(q.getSubject(), "SBB가 무엇인가요?");
+
+//		<< Update Data >>
+		Optional<Question> oq1 = qRepo.findById(1);
+		assertTrue(oq1.isPresent());
+		Question q1 = oq1.get();
+		q1.setSubject("수정된 제목");
+		qRepo.save(q1);
 	}
 }
