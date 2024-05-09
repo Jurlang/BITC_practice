2ce335fcf34d1725c802fc6b90a8cfd82fd8af6e
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2024-05-09 16:39:38 +0900

    boot3 SSB Example  - add Controller connect question_list.html  - question_list - table 표 작성  - Controller - 데이터 받아서 웹으로 던짐

--- JavaWeb/boot03/src/main/java/org/bitcprac/boot03/question/QuestionController.java
+++ JavaWeb/boot03/src/main/java/org/bitcprac/boot03/question/QuestionController.java
@@ -1,13 +1,23 @@
 package org.bitcprac.boot03.question;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/question")
 public class QuestionController {
+
+	@Autowired
+	private QuestionRepository qRepo;
+
 	@RequestMapping("/list")
-	public String questionList(){
+	public String questionList(Model model){
+		List<Question> qList = qRepo.findAll();
+		model.addAttribute("qList", qList);
 		return "question_list";
 	}
 }
