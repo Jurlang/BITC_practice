f6073bd8950591fe0cbcef98e3c667fc29487d9d
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2024-05-09 12:19:49 +0900

    boot3 SSB Example  - project setting

--- JavaWeb/boot03/src/main/java/org/bitcprac/boot03/Boot03Application.java
+++ JavaWeb/boot03/src/main/java/org/bitcprac/boot03/Boot03Application.java
@@ -1,13 +1,13 @@
-package org.bitcprac.boot3;
+package org.bitcprac.boot03;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Boot3Application {
+public class Boot03Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Boot3Application.class, args);
+		SpringApplication.run(Boot03Application.class, args);
 	}
 
 }
--- JavaWeb/boot03/src/test/java/org/bitcprac/boot03/Boot03ApplicationTests.java
+++ JavaWeb/boot03/src/test/java/org/bitcprac/boot03/Boot03ApplicationTests.java
@@ -1,10 +1,10 @@
-package org.bitcprac.boot3;
+package org.bitcprac.boot03;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Boot3ApplicationTests {
+class Boot03ApplicationTests {
 
 	@Test
 	void contextLoads() {
