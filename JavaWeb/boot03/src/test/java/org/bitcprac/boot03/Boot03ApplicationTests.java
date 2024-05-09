f6633a8cc33b1c070cb9fddf38bc6b259e122891
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2024-05-09 15:00:05 +0900

    boot3 SSB Example  -  Jpa Test

--- JavaWeb/boot03/src/test/java/org/bitcprac/boot03/Boot03ApplicationTests.java
+++ JavaWeb/boot03/src/test/java/org/bitcprac/boot03/Boot03ApplicationTests.java
@@ -1,13 +1,28 @@
 package org.bitcprac.boot03;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
 
 @SpringBootTest
 class Boot03ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private QuestionRepository qRepo;
 
+	@Test
+	void testJpa(){
+		Question q1 = new Question();
+		q1.setSubject("SBB가 무엇인가요?");
+		q1.setContent("SBB에 대해서 알고 싶습니다.");
+		q1.setCreateDate(LocalDateTime.now());
+		qRepo.save(q1);
+		Question q2 = new Question();
+		q2.setSubject("스프링부트 모델 질문입니다.");
+		q2.setContent("id는 자동으로 생성되나요?");
+		q2.setCreateDate(LocalDateTime.now());
+		qRepo.save(q2);
+	}
 }
