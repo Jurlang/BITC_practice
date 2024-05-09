77d32fed913e7f3f92f37754f7803d9b68ad1e4f
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2024-05-09 15:19:44 +0900

    boot3 SSB Example  -  QuestionRepo Add Custom Query

--- JavaWeb/boot03/src/main/java/org/bitcprac/boot03/QuestionRepository.java
+++ JavaWeb/boot03/src/main/java/org/bitcprac/boot03/QuestionRepository.java
@@ -3,4 +3,5 @@ package org.bitcprac.boot03;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
+	Question findBySubject(String subject);
 }
