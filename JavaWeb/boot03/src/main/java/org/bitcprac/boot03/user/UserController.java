ddb787f86abba811091a73331ed78f644fe1616d
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2024-05-13 15:53:08 +0900

    boot3 SSB Example  - UserController 생성  + 회원가입 로직 완료.

--- JavaWeb/boot03/src/main/java/org/bitcprac/boot03/user/UserController.java
+++ JavaWeb/boot03/src/main/java/org/bitcprac/boot03/user/UserController.java
@@ -1,10 +1,17 @@
 package org.bitcprac.boot03.user;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
 	@Autowired
 	private UserService uService;
@@ -12,5 +19,31 @@ public class UserController {
 	@GetMapping("/signup")
 	public String signup(UserCreateForm ucf) {
 		return "signup_form";
+	}
+
+	@PostMapping("/signup")
+	public String signup(@Valid @ModelAttribute("userCreateForm") UserCreateForm ucf, BindingResult result) {
+		if(result.hasErrors()) {
+			return "signup_form";
+		}
+		if (!ucf.getPassword1().equals(ucf.getPassword2())) {
+			result.rejectValue("password2", "passwordInCorrect",
+					"2개의 패스워드가 일치하지 않습니다.");
+			return "signup_form";
+		}
+
+		try {
+			uService.createUser(ucf.getUsername(), ucf.getEmail(), ucf.getPassword1());
+		} catch (DataIntegrityViolationException e) {
+			e.printStackTrace();
+			result.reject("signupFailed", "이미 등록된 사용자입니다.");
+			return "signup_form";
+		} catch (Exception e) {
+			e.printStackTrace();
+			result.reject("signupFailed", e.getMessage());
+			return "signup_form";
+		}
+
+		return "redirect:/";
 	}
 }
