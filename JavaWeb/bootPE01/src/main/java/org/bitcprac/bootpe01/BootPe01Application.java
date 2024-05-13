f4ae12a88ff979a3ccae47338e9c6c799d722b4f
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2024-05-13 12:16:15 +0900

    bootPE01 - Thymeleaf-layout Practice - 프로젝트 생성.

---
+++ JavaWeb/bootPE01/src/main/java/org/bitcprac/bootpe01/BootPe01Application.java
@@ -0,0 +1,13 @@
+package org.bitcprac.bootpe01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootPe01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootPe01Application.class, args);
+	}
+
+}
