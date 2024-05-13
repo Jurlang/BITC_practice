85004243f1bbe4889a00a14b9b57ddf4044fa055
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2024-05-13 14:36:26 +0900

    boot3 SSB Example  - UserService 생성

---
+++ JavaWeb/boot03/src/main/java/org/bitcprac/boot03/user/UserService.java
@@ -0,0 +1,23 @@
+package org.bitcprac.boot03.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository uRepo;
+
+	public SiteUser createUser(String username, String password, String email){
+		SiteUser siteUser = new SiteUser();
+		siteUser.setUsername(username);
+		siteUser.setEmail(email);
+
+		BCryptPasswordEncoder pwEncoder = new BCryptPasswordEncoder();
+		siteUser.setPassword(pwEncoder.encode(password));
+
+		return uRepo.save(siteUser);
+	}
+}
