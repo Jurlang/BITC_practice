f4a295cc7223164cd4ab15915d9965d8a3c5b841
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2024-05-13 15:19:39 +0900

    boot3 SSB Example  - 회원가입 양식 객체 UserCreateForm 생성  + ToString 어노테이션 추가

--- JavaWeb/boot03/src/main/java/org/bitcprac/boot03/user/UserCreateForm.java
+++ JavaWeb/boot03/src/main/java/org/bitcprac/boot03/user/UserCreateForm.java
@@ -6,9 +6,11 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class UserCreateForm {
 	@Size(min = 2, max = 25)
 	@NotBlank(message = "UserName 은 필수 항목입니다.")
