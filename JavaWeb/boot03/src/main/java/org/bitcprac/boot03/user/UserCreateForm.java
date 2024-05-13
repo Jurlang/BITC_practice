070338008de8e87348c91e087bb33f00d7916dbf
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2024-05-13 15:06:16 +0900

    boot3 SSB Example  - 회원가입 양식 객체 UserCreateForm 생성

---
+++ JavaWeb/boot03/src/main/java/org/bitcprac/boot03/user/UserCreateForm.java
@@ -0,0 +1,27 @@
+package org.bitcprac.boot03.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserCreateForm {
+	@Size(min = 2, max = 25)
+	@NotBlank(message = "UserName 은 필수 항목입니다.")
+	private String username;
+
+	@Size(min = 6)
+	@NotEmpty(message = "Password 는 필수 항목 입니다.")
+	private String password1;
+
+	@NotEmpty(message = "Password 확인은 필수 항목 입니다.")
+	private String password2;
+
+	@NotEmpty(message = "Email 은 필수 항목 입니다.")
+	@Email(message = "Email 양식에 맞게 적어주세요.")
+	private String email;
+}
