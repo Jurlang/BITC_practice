1096f47b3e66856a15e78fdffc4a6ceb71bc222b
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2024-05-14 11:41:22 +0900

    bootPE01 thymeleaf LAYOUT 개인공부

--- JavaWeb/bootPE01/src/main/java/org/bitcprac/bootpe01/TestController.java
+++ JavaWeb/bootPE01/src/main/java/org/bitcprac/bootpe01/TestController.java
@@ -12,4 +12,14 @@ public class TestController {
 		model.addAttribute("title", "ㅅㄷㄴㅅ");
 		return "pages/test";
 	}
+	@GetMapping("/2")
+	public String index2(Model model){
+		model.addAttribute("title", "qwer");
+		return "pages/test2";
+	}
+
+	@GetMapping("/sample/plain")
+	public void plain() {
+		System.out.println("/sample/plain");
+	}
 }
