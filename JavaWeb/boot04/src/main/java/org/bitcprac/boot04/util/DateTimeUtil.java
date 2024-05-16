e623c3526d19ed31535e475dc1d57dcc45d22b45
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2024-05-16 17:25:50 +0900

    Boot04 비용관리 프로젝트(Util) + Date ↔ String 변환 Util 구현

--- JavaWeb/boot04/src/main/java/org/bitcprac/boot04/util/DateTimeUtil.java
+++ JavaWeb/boot04/src/main/java/org/bitcprac/boot04/util/DateTimeUtil.java
@@ -1,11 +1,20 @@
 package org.bitcprac.boot04.util;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DateTimeUtil {
-	public static String convertDateToString(Date date){
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 MM월 dd일");
+	//sql날짜 => 문자열날짜
+	public static String convertDateToString(Date date) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 		return sdf.format(date);
+	}
+
+	//문자열날짜 => sql날짜
+	public static Date convertStringToDate(String dateString) throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		java.util.Date utilDate = sdf.parse(dateString);
+		return new Date(utilDate.getTime());
 	}
 }
