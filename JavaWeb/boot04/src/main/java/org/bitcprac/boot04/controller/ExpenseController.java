4bb2cf26ef4bb902e8fd2d99b6f930fd1fd462f6
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2024-05-16 15:15:58 +0900

    Boot04 비용관리 프로젝트 - Controller, html 연결 - List 출력

--- JavaWeb/boot04/src/main/java/org/bitcprac/boot04/controller/ExpenseController.java
+++ JavaWeb/boot04/src/main/java/org/bitcprac/boot04/controller/ExpenseController.java
@@ -1,13 +1,24 @@
 package org.bitcprac.boot04.controller;
 
+import lombok.AllArgsConstructor;
+import org.bitcprac.boot04.dto.ExpenseDTO;
+import org.bitcprac.boot04.service.ExpenseService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@AllArgsConstructor
 public class ExpenseController {
 
+	private final ExpenseService expService;
+
 	@GetMapping({"/expenses", "/"})
-	public String showExpenseList(){
+	public String showExpenseList(Model model){
+		List<ExpenseDTO> expList = expService.getAllExpenses();
+		model.addAttribute("expList", expList);
 		return "expenses-list";
 	}
 }
