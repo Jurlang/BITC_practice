a71e5c430a4e1d8734d6a36a1629c3b0cc548ce4
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2024-05-16 14:47:55 +0900

    Boot04 비용관리 프로젝트 - Service + ModelMapper 연결

--- JavaWeb/boot04/src/main/java/org/bitcprac/boot04/service/ExpenseService.java
+++ JavaWeb/boot04/src/main/java/org/bitcprac/boot04/service/ExpenseService.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.bitcprac.boot04.dto.ExpenseDTO;
 import org.bitcprac.boot04.entity.Expense;
 import org.bitcprac.boot04.repository.ExpenseRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,23 +14,20 @@ import java.util.stream.Collectors;
 @AllArgsConstructor
 public class ExpenseService {
 
+	private final ModelMapper modelMapper;
 	private ExpenseRepository eRepo;
+
+	private ExpenseDTO mapToDTO(Expense expense){
+//		ExpenseDTO expenseDTO = modelMapper.map(expense, ExpenseDTO.class);
+//		return expenseDTO;
+		return modelMapper.map(expense, ExpenseDTO.class);
+	}
 
 	public List<ExpenseDTO> getAllExpenses(){
 		List<Expense> list = eRepo.findAll();
-		List<ExpenseDTO> listDto = list.stream().map(this::mapToDTO).collect(Collectors.toList());
-		return listDto;
-	}
-
-	private ExpenseDTO mapToDTO(Expense expense){
-		ExpenseDTO dto = new ExpenseDTO();
-		dto.setId(expense.getId());
-		dto.setExpenseId(expense.getExpenseId());
-		dto.setAmount(expense.getAmount());
-		dto.setName(expense.getName());
-		dto.setDescription(expense.getDescription());
-		dto.setDate(expense.getDate());
-		return dto;
+//		List<ExpenseDTO> listDto = list.stream().map(this::mapToDTO).collect(Collectors.toList());
+//		return listDto;
+		return list.stream().map(this::mapToDTO).collect(Collectors.toList());
 	}
 
 }
