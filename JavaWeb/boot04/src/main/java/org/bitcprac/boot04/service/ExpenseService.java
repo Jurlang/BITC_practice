3ec4a3c1377de5fa18c8a6048142fc1bf5b45cf9
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2024-05-16 17:25:19 +0900

    Boot04 비용관리 프로젝트(Service) + ModelMapper 연결 + Mapper 변환(vo - dto) 시 Date->String 추가 + Mapper 변환(dto - vo) 시 String -> Date 추가 및 UUID 추가 - 리스트 로직 구현 - 등록 로직 구현

--- JavaWeb/boot04/src/main/java/org/bitcprac/boot04/service/ExpenseService.java
+++ JavaWeb/boot04/src/main/java/org/bitcprac/boot04/service/ExpenseService.java
@@ -1,6 +1,5 @@
 package org.bitcprac.boot04.service;
 
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.bitcprac.boot04.dto.ExpenseDTO;
 import org.bitcprac.boot04.entity.Expense;
@@ -9,7 +8,9 @@ import org.bitcprac.boot04.util.DateTimeUtil;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.text.ParseException;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
@@ -24,10 +25,23 @@ public class ExpenseService {
 		expenseDTO.setDateString(DateTimeUtil.convertDateToString(expenseDTO.getDate()));
 		return expenseDTO;
 	}
+	private Expense mapToEntity(ExpenseDTO expenseDTO) throws ParseException {
+		Expense expense = modelMapper.map(expenseDTO, Expense.class);
+		expense.setExpenseId(UUID.randomUUID().toString());
+		expense.setDate(DateTimeUtil.convertStringToDate(expenseDTO.getDateString()));
+		return expense;
+	}
 
 	public List<ExpenseDTO> getAllExpenses(){
 		List<Expense> list = eRepo.findAll();
 		return list.stream().map(this::mapToDTO).collect(Collectors.toList());
 	}
+
+	public ExpenseDTO saveExpenseDetails(ExpenseDTO expenseDTO) throws ParseException {
+		Expense expense = mapToEntity(expenseDTO);
+		expense = eRepo.save(expense);
+		return mapToDTO(expense);
+	}
+
 
 }
