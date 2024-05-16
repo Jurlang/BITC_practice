e76e429adee32a7095a57b3f29937ec580cf1a85
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2024-05-16 17:23:34 +0900

    Boot04 비용관리 프로젝트(DTO) - 기본 정보 등록 - Validation 및 ToString 어노테이션 등록

--- JavaWeb/boot04/src/main/java/org/bitcprac/boot04/dto/ExpenseDTO.java
+++ JavaWeb/boot04/src/main/java/org/bitcprac/boot04/dto/ExpenseDTO.java
@@ -1,27 +1,35 @@
 package org.bitcprac.boot04.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.Date;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class ExpenseDTO {
 
 	private long id;
 
 	private String expenseId;
 
+	@NotBlank(message = "이름은 필수 항목 입니다.")
 	private String name;
 
 	private String description;
 
+	@Min(value = 100, message = "100원 이상 등록 가능합니다.")
+	@NotNull(message = "비용은 필수 항목 입니다.")
 	private long amount;
+
 
 	private Date date;
 
+	@NotEmpty(message = "날짜는 필수 항목 입니다.")
 	private String dateString;
 }
