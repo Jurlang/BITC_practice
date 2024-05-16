3b41d95d6b03590b35fa3348405bea19afb7c0bc
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2024-05-16 17:22:24 +0900

    Boot04 비용관리 프로젝트(controller) - Controller, html 연결 - List 출력 - 생성자 주입 ( allargs -> requiredargs 변경 ) - 등록 (get , post ) 추가

--- JavaWeb/boot04/src/main/java/org/bitcprac/boot04/controller/ExpenseController.java
+++ JavaWeb/boot04/src/main/java/org/bitcprac/boot04/controller/ExpenseController.java
@@ -1,13 +1,17 @@
 package org.bitcprac.boot04.controller;
 
-import lombok.AllArgsConstructor;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.bitcprac.boot04.dto.ExpenseDTO;
 import org.bitcprac.boot04.service.ExpenseService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import java.text.ParseException;
 import java.util.List;
 
 @Controller
@@ -21,5 +25,19 @@ public class ExpenseController {
 		List<ExpenseDTO> expList = expService.getAllExpenses();
 		model.addAttribute("expList", expList);
 		return "expenses-list";
+	}
+
+	@GetMapping({"/createExpense"})
+	public String createExpense(@ModelAttribute("expenseDto") ExpenseDTO expenseDto, Model model){
+		return "expenses-form";
+	}
+	@PostMapping("/createExpense")
+	public String createExpense(@Valid @ModelAttribute("expenseDto") ExpenseDTO expenseDto, BindingResult result) throws ParseException {
+		if(result.hasErrors()){
+			return "expenses-form";
+		}
+		expService.saveExpenseDetails(expenseDto);
+		return "redirect:/expenses";
+
 	}
 }
