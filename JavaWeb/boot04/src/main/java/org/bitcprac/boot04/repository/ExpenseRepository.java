577e8dedad443dadd931f2caaa9b9a430a52b555
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2024-05-20 10:27:23 +0900

    Boot04 비용관리 프로젝트(Repo) - expenseId 를 통해 찾기 - 이름을 통해 찾기   > 이름 찾기 -> 이름과 날짜로 찾기 변경

--- JavaWeb/boot04/src/main/java/org/bitcprac/boot04/repository/ExpenseRepository.java
+++ JavaWeb/boot04/src/main/java/org/bitcprac/boot04/repository/ExpenseRepository.java
@@ -3,10 +3,11 @@ package org.bitcprac.boot04.repository;
 import org.bitcprac.boot04.entity.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
 	Optional<Expense> findByExpenseId(String expenseId);
-	List<Expense> findByNameContaining(String keyword);
+	List<Expense> findByNameContainingAndDateBetween(String keyword, Date start, Date end);
 }
