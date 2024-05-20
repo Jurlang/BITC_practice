708c4dd777525f01dee52b8d02671caba1bef63a
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2024-05-20 16:36:48 +0900

    Boot04 비용관리 프로젝트(config) - 보안, 비밀번호 암호화

---
+++ JavaWeb/boot04/src/main/java/org/bitcprac/boot04/config/CustomSecurityConfig.java
@@ -0,0 +1,31 @@
+package org.bitcprac.boot04.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class CustomSecurityConfig {
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+				.csrf(AbstractHttpConfigurer::disable)
+				.authorizeHttpRequests(
+						(authz)->authz
+								.requestMatchers("/js/**", "/css/**").permitAll()
+								.requestMatchers("/","/login","/register").permitAll()
+				);
+
+		return http.build();
+	}
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
+}
