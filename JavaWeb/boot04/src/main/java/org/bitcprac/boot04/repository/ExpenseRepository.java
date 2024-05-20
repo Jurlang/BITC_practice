691efe5114b952cc1f30f378617c84d3913486b7
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2024-05-20 10:06:30 +0900

    Boot04 비용관리 프로젝트(Repo) - expenseId 를 통해 찾기 - 이름을 통해 찾기

--- JavaWeb/boot04/src/main/java/org/bitcprac/boot04/repository/ExpenseRepository.java
+++ JavaWeb/boot04/src/main/java/org/bitcprac/boot04/repository/ExpenseRepository.java
@@ -3,6 +3,10 @@ package org.bitcprac.boot04.repository;
 import org.bitcprac.boot04.entity.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+import java.util.List;
+import java.util.Optional;
 
+public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+	Optional<Expense> findByExpenseId(String expenseId);
+	List<Expense> findByNameContaining(String keyword);
 }
