8d55577787cc6ef94016e0a418d131de1720386b
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2024-05-20 11:03:14 +0900

    Boot04 비용관리 프로젝트(DTO) - 기본 정보 등록 - Validation 및 ToString 어노테이션 등록 + 검색용 DTO 파일 추가 ( filterDTO ) + 검색용 DTO - 날짜 시작, 종료 추가

--- JavaWeb/boot04/src/main/java/org/bitcprac/boot04/dto/ExpenseFilterDTO.java
+++ JavaWeb/boot04/src/main/java/org/bitcprac/boot04/dto/ExpenseFilterDTO.java
@@ -3,7 +3,9 @@ package org.bitcprac.boot04.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+@ToString
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
