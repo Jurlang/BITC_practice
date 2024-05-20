03b56dfff97790412c535cb8b2f5662414f225a7
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2024-05-20 16:16:47 +0900

    Boot04 비용관리 프로젝트(controller) - User 관련 컨트롤러 추가

---
+++ JavaWeb/boot04/src/main/java/org/bitcprac/boot04/controller/AuthController.java
@@ -0,0 +1,42 @@
+package org.bitcprac.boot04.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.bitcprac.boot04.dto.UserDTO;
+import org.bitcprac.boot04.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class AuthController {
+
+	private final UserService userService;
+
+	@GetMapping({"/login", "/"})
+	public String showLoginPage(){
+		return "login";
+	}
+
+	@GetMapping("/register")
+	public String showRegisterPage(Model model){
+		model.addAttribute("user", new UserDTO());
+		return "register";
+	}
+	@PostMapping("/register")
+	public String registerPage(@Valid @ModelAttribute("user") UserDTO userDto, BindingResult result, Model model){
+
+		if(result.hasErrors()){
+			return "register";
+		}
+
+		userService.save(userDto);
+
+		model.addAttribute("successMSG", true);
+		return "login";
+	}
+}
