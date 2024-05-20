3ee57722a33c5c3f4849a64045cc43e67eb6e3a4
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2024-05-20 16:37:19 +0900

    Boot04 비용관리 프로젝트(Service) - User - 저장 시 비밀번호 암호화 추가

--- JavaWeb/boot04/src/main/java/org/bitcprac/boot04/service/UserService.java
+++ JavaWeb/boot04/src/main/java/org/bitcprac/boot04/service/UserService.java
@@ -5,6 +5,7 @@ import org.bitcprac.boot04.dto.UserDTO;
 import org.bitcprac.boot04.entity.User;
 import org.bitcprac.boot04.repository.UserRepository;
 import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -15,12 +16,14 @@ public class UserService {
 
 	private final UserRepository uRepo;
 	private final ModelMapper modelMapper;
+	private final PasswordEncoder passwordEncoder;
 
 	public User mapToEntity(UserDTO dto) {
 		return modelMapper.map(dto, User.class);
 	}
 
 	public void save(UserDTO userDTO){
+		userDTO.setPassword(passwordEncoder.encode(userDTO.getPassword()));
 		User user = mapToEntity(userDTO);
 		user.setUserId(UUID.randomUUID().toString());
 		uRepo.save(user);
