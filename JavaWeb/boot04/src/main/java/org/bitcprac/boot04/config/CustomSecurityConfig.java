d13a50f4188cd4205444112a524c807419a87317
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2024-05-21 09:00:34 +0900

    Boot04 비용관리 프로젝트(config) - 보안, 비밀번호 암호화 - 보안 안하는 page 따로 구현 - 로그인 구현

--- JavaWeb/boot04/src/main/java/org/bitcprac/boot04/config/CustomSecurityConfig.java
+++ JavaWeb/boot04/src/main/java/org/bitcprac/boot04/config/CustomSecurityConfig.java
@@ -1,24 +1,40 @@
 package org.bitcprac.boot04.config;
 
+import lombok.RequiredArgsConstructor;
+import org.bitcprac.boot04.service.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class CustomSecurityConfig {
+
+	@SuppressWarnings("unused")
+	private final CustomUserDetailsService customUserDetailsService;
+
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
 				.csrf(AbstractHttpConfigurer::disable)
 				.authorizeHttpRequests(
 						(authz)->authz
-								.requestMatchers("/js/**", "/css/**").permitAll()
 								.requestMatchers("/","/login","/register").permitAll()
+				)
+				.formLogin((formLogin) -> formLogin
+						.loginPage("/login")
+						.failureUrl("/login")
+						.defaultSuccessUrl("/expenses")
+						.usernameParameter("email")
+						.passwordParameter("password")
 				);
 
 		return http.build();
@@ -27,5 +43,16 @@ public class CustomSecurityConfig {
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder(){
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public WebSecurityCustomizer webSecurityCustomizer(){
+		return (web) -> web
+				.ignoring().requestMatchers("/css/**", "/js/**", "/error");
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
 	}
 }
