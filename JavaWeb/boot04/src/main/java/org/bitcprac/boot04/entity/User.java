57cb4c3f40333b97e79b33fbdaf32734ccd51a3e
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2024-05-21 09:00:56 +0900

    Boot04 비용관리 프로젝트(Entity) - User

--- JavaWeb/boot04/src/main/java/org/bitcprac/boot04/entity/User.java
+++ JavaWeb/boot04/src/main/java/org/bitcprac/boot04/entity/User.java
@@ -4,12 +4,14 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Table(name="tbl_users")
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@ToString
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
