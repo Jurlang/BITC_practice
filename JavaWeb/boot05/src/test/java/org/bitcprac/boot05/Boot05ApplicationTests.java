84c311a7440f92fee953200002d3bb40e1e8d0dc
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2024-05-22 09:15:37 +0900

    Boot05 - SpringBoot MyBatis

---
+++ JavaWeb/boot05/src/test/java/org/bitcprac/boot05/Boot05ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bitcprac.boot05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot05ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
