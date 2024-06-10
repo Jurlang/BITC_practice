d01b248c22ed68c18776c69d267de74a4bfd750a
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2024-06-10 14:36:12 +0900

    JSX 문법 ( 자바스크립트를 같이 쓰는 리액트 문법 )

---
+++ React/first-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+	<div>
+	  <h1>처음 앱</h1>
+	  <p>현재 시간 : {new Date().toLocaleString()}.</p>
+	  <small>@카피라이트</small>
+	</div>
+  );
+}
+
+export default App;
