4698f161188f69993ec6e4126bcd5ba6da58b2d0
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2024-06-12 13:58:38 +0900

    카드뒤집기(Magic Memory) 완성 !

--- React/magic-memory/src/App.js
+++ React/magic-memory/src/App.js
@@ -1,12 +1,87 @@
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import SingleCard from "./component/SingleCard";
+
+const cardImages = [
+    { src: "/img/helmet-1.png", matched: false }, 
+    { src: "/img/potion-1.png", matched: false }, 
+    { src: "/img/ring-1.png", matched: false }, 
+    { src: "/img/scroll-1.png", matched: false }, 
+    { src: "/img/shield-1.png", matched: false }, 
+    { src: "/img/sword-1.png", matched: false }
+];
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Magic Match</h1>
-      <button>New Game</button>
-    </div>
-  );
+
+    const [cards, setCards] = useState([]);
+    const [turns, setTurns] = useState(0);
+    const [choiceOne, setChoiceOne] = useState(null);
+    const [choiceTwo, setChoiceTwo] = useState(null);
+    const [disabled, setDisabled] = useState(false);
+
+    const shuffleCards = () => {
+        const randomCards = [...cardImages, ...cardImages]
+                            .sort(() => Math.random() - 0.5)
+                            .map((card) => ({ ...card, id: Math.random() }));
+
+        setChoiceOne(null);
+        setChoiceTwo(null);
+        setCards(randomCards);
+        setTurns(0);
+    };
+
+    const handleChoice = (card) => {
+        choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+    }
+
+    useEffect( () => {
+        shuffleCards();
+    }, []);
+
+    useEffect( () => {
+        if (choiceOne && choiceTwo){
+            setDisabled(true);
+            if (choiceOne.src === choiceTwo.src){
+                setCards(prevCards => {
+                    return prevCards.map(card=>{
+                        if ( card.src === choiceOne.src) {
+                            return {...card, matched: true};
+                        } else {
+                            return card;
+                        }
+                    });
+                });
+                resetTurn();
+            } else {
+                setTimeout(() => resetTurn(), 500);
+            }
+        }
+    },[choiceOne, choiceTwo]);
+
+    const resetTurn = () => {
+        setChoiceOne(null);
+        setChoiceTwo(null);
+        setTurns( (prev) => prev + 1);
+        setDisabled(false);
+    }
+    return (
+        <div className="App">
+            <h1>Magic Match</h1>
+            <button onClick={shuffleCards}>New Game</button>
+            <div className='card-grid'>
+            {cards.map(card=>(
+                <SingleCard 
+                        handleChoice={handleChoice} 
+                        card={card} 
+                        key={card.id} 
+                        flipped={card === choiceOne || card === choiceTwo || card.matched} 
+                        disabled={disabled}
+                />
+            ))}
+            </div>
+            <p>턴수 : {turns}</p>
+        </div>
+    );
 }
 
-export default App
+export default App;
---
+++ React/magic-memory/src/component/SingleCard.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./SingleCard.css";
+
+const SingleCard = ({card, handleChoice, flipped, disabled}) => {
+
+	function handleClick() {
+		if(!disabled){
+			handleChoice(card);
+		}
+	}
+
+    return (
+        <div className="card">
+            <div className={flipped ? 'flipped' : ''}>
+				<img className='front' src={card.src} alt='card front'></img>
+				<img className='back' src='/img/cover.png' alt='card back' onClick={handleClick}></img>
+			</div>
+        </div>
+    );
+};
+
+export default SingleCard;
