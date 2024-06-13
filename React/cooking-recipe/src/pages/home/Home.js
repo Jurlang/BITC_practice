4d9dcc4c983981bf337cff04af9fefb16b60a574
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2024-06-13 15:21:23 +0900

    useFetch 라이브러리를 사용해 recipe 데이터를 불러와서 home 에 보이기.

--- React/cooking-recipe/src/pages/home/Home.js
+++ React/cooking-recipe/src/pages/home/Home.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import './Home.css'
+import { useFetch } from '../../hooks/useFetch'
 
 const Home = () => {
+	// useFetch 를 사용해서 가져오는 데이터 : data isPending, error
+	 const { data, isPending, error }= useFetch('http://localhost:3030/recipes');
   return (
-	<div>
-	  Home
+	<div className='home'>
+		{error && <p className="error">{error}</p>}
+		{isPending && <p className="loading">로딩중 ...</p>}
+		{data && data.map(recipe => <h2 key={recipe.id}>{recipe.title}</h2>)}
 	</div>
   )
 }
