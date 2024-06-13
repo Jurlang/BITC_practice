83e8846a1379b4701093eb906d2430636fb57182
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2024-06-13 14:19:09 +0900

    기본 설정 및 json db 설정

--- React/cooking-recipe/src/App.js
+++ React/cooking-recipe/src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-    return <div className="App">앱시작</div>;
+    return (
+    <div className="App">
+        앱시작
+    </div>
+    );
 }
 
 export default App;
