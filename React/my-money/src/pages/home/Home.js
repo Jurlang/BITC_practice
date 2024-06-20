52a7b64113812a587eb8dc876ef0392989ccd732
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2024-06-20 11:53:52 +0900

    홈 화면 만들기

--- React/my-money/src/pages/home/Home.js
+++ React/my-money/src/pages/home/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
 import styles from "./Home.module.css";
+import TransactionForm from "../../components/TransactionForm";
 
 export default function Home() {
     return (
-        <div>
-            <div>Home</div>
+        <div className={styles.container}>
+            <div className={styles.content}>거래내역</div>
+            <div className={styles.sidebar}>
+                <TransactionForm />
+            </div>
         </div>
     );
 }
