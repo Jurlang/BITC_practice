3604ed6f47871a1f02db3b2ff3f37555035f4eb5
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2024-06-24 14:20:46 +0900

    프로젝트 세팅

---
+++ React/movie-view/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='app'>
+      <nav className='navbar'>Navbar content</nav>
+
+      <main>Main content</main>
+    </div>
+  )
+}
+
+export default App
