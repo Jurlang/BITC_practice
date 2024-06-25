0c5909adf961e2222d2f3933b6891a50029dada2
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2024-06-26 08:54:33 +0900

    개인 프로젝트 생성

---
+++ React/self-todo/src/App.jsx
@@ -0,0 +1,12 @@
+import InsertForm from "./components/InsertForm";
+import "bootstrap/dist/css/bootstrap.min.css"
+const App = () => {
+  return (
+    <div>
+      <button className='btn btn-primary'>button</button>
+      <InsertForm />
+    </div>
+  );
+};
+
+export default App;
---
+++ React/self-todo/src/components/InsertForm.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+const InsertForm = () => {
+  const [task, setTask] = useState("");
+
+  return (
+    <div>
+      다음에 꼭 하기
+    </div>
+  );
+};
+
+export default InsertForm;
