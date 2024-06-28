e38876b6b4fd1fc8bb04d0fe13c2e9ebff37927a
Practice_JMH_Git <42147463+Jurlang@users.noreply.github.com>
2024-06-28 12:21:37 +0900

    로그인 안했을때 장바구니 및 주문확인 페이지 접속 막기

---
+++ React/myCart/src/components/Routing/ProtectedRoute.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable react/prop-types */
+import { Navigate, Outlet } from "react-router-dom"
+
+const ProtectedRoute = ({user}) => {
+	return user ? <Outlet /> : <Navigate to='/login'/>;
+};
+
+export default ProtectedRoute;
--- React/myCart/src/components/Routing/Routing.jsx
+++ React/myCart/src/components/Routing/Routing.jsx
@@ -1,28 +1,31 @@
 /* eslint-disable react/prop-types */
-import HomePage from '../Home/HomePage';
-import ProductsPage from '../Products/ProductsPage';
-import CartPage from '../Cart/CartPage';
-import MyOrderPage from '../MyOrder/MyOrderPage';
-import LoginPage from '../Authentication/LoginPage';
-import SingleProductPage from '../SingleProductPage/SingleProductPage';
-import SignupPage from '../Authentication/SignupPage';
+import HomePage from "../Home/HomePage";
+import ProductsPage from "../Products/ProductsPage";
+import CartPage from "../Cart/CartPage";
+import MyOrderPage from "../MyOrder/MyOrderPage";
+import LoginPage from "../Authentication/LoginPage";
+import SingleProductPage from "../SingleProductPage/SingleProductPage";
+import SignupPage from "../Authentication/SignupPage";
 
-import { Route, Routes } from 'react-router-dom';
-import Logout from '../Authentication/Logout';
+import { Navigate, Route, Routes } from "react-router-dom";
+import Logout from "../Authentication/Logout";
+import ProtectedRoute from "./ProtectedRoute";
 
-const Routing = () => {
-	return (
-		<Routes>
-			<Route path='/' element={<HomePage/>}/>
-			<Route path='/products' element={<ProductsPage/>}/>
-			<Route path='/products/:id' element={<SingleProductPage/>}/>
-			<Route path='/signup' element={<SignupPage/>}/>
-			<Route path='/login' element={<LoginPage/>}/>
-			<Route path='/cart' element={<CartPage />}/>
-			<Route path='/myorders' element={<MyOrderPage/>}/>
-			<Route path='/logout' element={<Logout />} />
-		</Routes>
-	)
-}
+const Routing = ({ user }) => {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/products" element={<ProductsPage />} />
+      <Route path="/products/:id" element={<SingleProductPage />} />
+      <Route path="/signup" element={user ? <Navigate to="/" /> : <SignupPage />} />
+      <Route path="/login" element={user ? <Navigate to="/" /> : <LoginPage />} />
+      <Route element={<ProtectedRoute />}>
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/myorders" element={<MyOrderPage />} />
+      </Route>
+    </Routes>
+  );
+};
 
-export default Routing
+export default Routing;
