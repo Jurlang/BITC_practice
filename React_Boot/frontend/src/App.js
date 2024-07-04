5cf7535e0608c51551c2f9081a38a548685ee061
jmh <whaksgus123@gmail.com>
2024-07-04 10:45:20 +0900

    라우트 추가

--- React_Boot/frontend/src/App.js
+++ React_Boot/frontend/src/App.js
@@ -1,17 +1,22 @@
 import Navbar from "./layout/Navbar";
+import AddUser from "./users/AddUser";
 
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import Home from "./pages/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Navbar />
-      <div className="container mt-5">
-        <Home />
-      </div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/adduser" element={<AddUser />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
