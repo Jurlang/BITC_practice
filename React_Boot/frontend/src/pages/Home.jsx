5157ddd5426eeeb6ae120ba1972cf8a2cf75a51c
jmh <whaksgus123@gmail.com>
2024-07-04 11:33:03 +0900

    삭제 구현

--- React_Boot/frontend/src/pages/Home.jsx
+++ React_Boot/frontend/src/pages/Home.jsx
@@ -14,6 +14,13 @@ const Home = () => {
     setUsers(result.data);
   };
 
+  const deleteUser = async (id) => {
+    if (window.confirm("정말로 삭제하시겠습니까?")) {
+      await axios.delete(`http://localhost:8080/api/users/${id}`);
+      loadUsers();
+    }
+  };
+
   return (
     <div className="mt-5 container">
       <table className="table text-center">
@@ -29,14 +36,29 @@ const Home = () => {
         <tbody className="table-group-divider">
           {users.map((user, index) => (
             <tr key={user.id}>
-              <th scope="row">{index}</th>
+              <th scope="row">{index+1}</th>
               <td>{user.username}</td>
               <td>{user.name}</td>
               <td>{user.email}</td>
               <td>
-                <Link to={`/readuser/${user.id}`} className="btn btn-outline-secondary mx-2">보기</Link>
-                <Link to={`/edituser/${user.id}`} className="btn btn-outline-warning mx-2">수정</Link>
-                <Link to={`/deleteuser/${user.id}`} className="btn btn-outline-danger mx-2">삭제</Link>
+                <Link
+                  to={`/readuser/${user.id}`}
+                  className="btn btn-outline-secondary mx-2"
+                >
+                  보기
+                </Link>
+                <Link
+                  to={`/edituser/${user.id}`}
+                  className="btn btn-outline-warning mx-2"
+                >
+                  수정
+                </Link>
+                <button
+                  onClick={() => deleteUser(user.id)}
+                  className="btn btn-outline-danger mx-2"
+                >
+                  삭제
+                </button>
               </td>
             </tr>
           ))}
