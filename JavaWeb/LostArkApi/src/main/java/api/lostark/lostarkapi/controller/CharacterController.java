b8ec4d1fa32b70edf407215e03e9257273961704
jmh <whaksgus123@gmail.com>
2024-08-08 17:29:20 +0900

    LostArk Api 개발 중.

--- JavaWeb/LostArkApi/src/main/java/api/lostark/lostarkapi/controller/CharacterController.java
+++ JavaWeb/LostArkApi/src/main/java/api/lostark/lostarkapi/controller/CharacterController.java
@@ -3,17 +3,27 @@ package api.lostark.lostarkapi.controller;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Log4j2
 @Controller
-@RequestMapping("/characterlist")
+@RequestMapping("/character")
 public class CharacterController {
 
-    @GetMapping("")
+    @GetMapping({"/list", "/list/{characterName}"})
     @PreAuthorize("hasRole('USER')")
     public String characterListGet() {
         return "/character/characterlist";
     }
+
+    @GetMapping("/{characterName}")
+    @PreAuthorize("hasRole('USER')")
+    public String character(@PathVariable String characterName, Model model) {
+        model.addAttribute("characterName", characterName);
+        return "/character/character";
+    }
 }
+
